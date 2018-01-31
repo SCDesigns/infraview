c10e19ab8bf59772f8796776d28735366aff4185
Sean Clarke <xseanclarke@gmail.com>
2018-01-30 20:55:00 -0500

    refactor: fixed navbar - dropdown working properly, jquery active page in progress

--- assets/js/menu.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  $("#hamburger").on("click", function(){
-    $("#toggle-list ul").toggleClass("open");
-  });
-});
--- portable-boiler-thermometer/about.php
+++ portable-boiler-thermometer/about.php
@@ -1,19 +1,33 @@
 <head>
   <link rel="stylesheet" href="../assets/css/style.css">
 </head>
+  <?php include "./nav/header-nav.html" ?>
 <body>
-  <h2 class="common-Uppercase">
-    About
-  </h2>
-  <p class="common-BodyText">
-    <span class="alpha">T</span>he Infra-View Boiler Thermometer also can be configured in a portable version, with a battery pack, digital display and Infra-View software for temperature data logging and boiler temperature profiling.
-  </p>
-  <p class="common-BodyText">
-    <span class="alpha"> Fig 1: </span>
-      Infra-View Compact Portable Infrared Boiler Thermometer in durable carrying case.
-  </p>
-  <p class="common-BodyText">
-    <span class="alpha"> Fig 2: </span>
-      Infra-View Compact Portable Infrared Boiler Thermometer with tabletop tripod, battery pack with digital display and customer supplied laptop.
-  </p>
+  <div class="link-row">
+    <div class="cols container-lg">
+      <div class="left" id="content">
+        <h2 class="common-Uppercase">
+          About
+        </h2>
+        <p class="common-BodyText">
+          <span class="alpha">T</span>he Infra-View Boiler Thermometer also can be configured in a portable version, with a battery pack, digital display and Infra-View software for temperature data logging and boiler temperature profiling.
+        </p>
+        <p class="common-BodyText">
+          <span class="alpha"> Fig 1: </span>
+            Infra-View Compact Portable Infrared Boiler Thermometer in durable carrying case.
+        </p>
+        <p class="common-BodyText">
+          <span class="alpha"> Fig 2: </span>
+            Infra-View Compact Portable Infrared Boiler Thermometer with tabletop tripod, battery pack with digital display and customer supplied laptop.
+        </p>
+      </div>
+      <div class="right" id="images">
+        <h2 class="common-Uppercase center">
+          Infra-View&reg; Portable Boiler Thermometer
+        </h2>
+        <?php include "./images/_default.html" ?>
+      </div>
+    </div>
+  </div>
 </body>
+<?php include "../partials/footer.html" ?>
